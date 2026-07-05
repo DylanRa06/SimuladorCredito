b6df9e098860b7c055a5d40ff61fd714b5e7637c
DylanRa06 <dylanramones20000@hotmail.com>
2026-07-05 16:54:14 -0500

    Parte 1 - Pasos 2 y 3: Enlace de scripts e implementacion de calcularDisponible con spnDisponible

--- simulador.js
+++ simulador.js
@@ -1,1 +1,12 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular() {
+    let componenteIngresos = document.getElementById("txtIngresos");
+    let componenteEgresos = document.getElementById("txtEgresos");
+
+    let ingresos = parseFloat(componenteIngresos.value);
+    let egresos = parseFloat(componenteEgresos.value);
+
+    let disponible = calcularDisponible(ingresos, egresos);
+
+    document.getElementById("spnDisponible").innerText = "USD " + disponible.toFixed(2);
+}
